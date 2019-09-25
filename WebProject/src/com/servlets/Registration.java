9111fa7d881180265f6befa88f6d470bbec9c953
M.I. RANA <M.I. RANA@192.168.1.12>
2019-09-25 02:49:02 -0700

    1st commit

---
+++ WebProject/src/com/servlets/Registration.java
@@ -0,0 +1,58 @@
+package com.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.models.RegModel;
+
+/**
+ * Servlet implementation class Registration
+ */
+@WebServlet("/Registration")
+public class Registration extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Registration() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String firstName=request.getParameter("firstNmae");
+		String lastName=request.getParameter("lastNmae");
+		String phone=request.getParameter("phone");
+		String email=request.getParameter("email");
+		
+		System.out.println("FirstName: "+firstName+"Lastname: "+lastName+"Phone:" +phone +"Email: " + email);
+		
+		RegModel regmodel=new RegModel();
+		
+		regmodel.setFirstName(firstName);
+		regmodel.setLastName(lastName);
+		regmodel.setPhone(phone);
+		regmodel.setEmail(email);
+		
+		
+		
+	}
+
+}
